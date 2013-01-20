70dc8f494377ee3909907a93bf33488672c0857e
Bryan Hanson <hanson@depauw.edu>
2013-01-19 21:05:59 -0500

    partially working formula interface

--- R/compareCats.R
+++ R/compareCats.R
@@ -1,7 +1,6 @@
 compareCats <-
-function(data = NULL, res = NULL,
-	fac1 = NULL, fac2 = NULL,
-	fac1cols = NULL, freckles = FALSE,
+function(formula = NULL, data = NULL,
+	cols = NULL, freckles = FALSE,
 	method = c("sem", "iqr", "mad", "box", "points", "sem95"),
 	xlab = NULL, ylab = NULL, title = NULL, ...)
 	{
@@ -10,67 +9,188 @@ function(data = NULL, res = NULL,
 # plotting means and se's or something similar
 # Bryan Hanson, DePauw Univ, Jan 2010
 
-	if (is.null(fac1cols)) stop("You need to supply fac1cols")
+	# Check and process the formula
 	
-	# 1st, reduce the df and clean it of NAs
+	if (is.null(formula)) stop("Formula not given")
+	if (!is.formula(formula)) stop("Invalid formula specification")
+
+	TwoFac <- FALSE
+	res <- as.character(formula[[2]])
+	if (length(formula[[3]]) == 1) {
+		fac1 <- as.character(formula[[3]])
+		}
+	if (length(formula[[3]]) == 3) {
+		fac2 <- as.character(formula[[3]][3])
+		fac1 <- as.character(formula[[3]][2])
+		TwoFac <- TRUE
+		}
+		
+	# Reduce the df and clean it of NAs
 	# otherwise there is trouble with the counts
 	
-	if (!identical(fac2, NULL)) {
-		keep <- c(res, fac1, fac2)
-		} else {
-			keep <- c(res, fac1)
-			}
+	if (TwoFac) keep <- c(res, fac1, fac2)
+	if (!TwoFac) keep <- c(res, fac1)
 	data <- data[, keep]
 	data <- na.omit(data)
 
-	# Specify the data & aesthetics
-		
-	if (identical(fac2, NULL)) { 
-		p <- ggplot(data, aes_string(x = fac1, y = res, color = fac1))
-		
-		} else {
-			p <- ggplot(data, aes_string(x = fac1, y = res, color = fac1)) +
-			facet_grid(paste(". ~", fac2, sep = ""))
-			}
+	if (!is.factor(data[,fac1])) stop("factor1 was not actually of type factor")
+	if (TwoFac) if (!is.factor(data[,fac2])) stop("factor2 was not actually of type factor")
 				
-	# now add layers as requested
+	# Initialize + add the points, jittered or not
+	# This is aesthetic #1
 	
+	p <- ggplot()
+
+	if (!freckles) {
+		if (TwoFac) {
+			p <- p + geom_point(data = data, 
+			aes_string(x = fac1, y = res, color = fac2, group = fac2))
+			# p <- ggplot(data = data, geom = "point",
+			# aes_string(x = fac1, y = res, color = fac2, group = fac2))
+			}
+		if (!TwoFac) {
+			p <- p + geom_point(data = data, 
+			aes_string(x = fac1, y = res, color = fac1))
+			}
+		}
+		
 	if (freckles) {
-		if (method == "points") warning("It doesn't make sense to combine points with freckles!")
-		jit <- position_jitter(width = 0.1, height = 0.0)
-		p <- p + layer(geom = "jitter", position = jit, color = "black", size = 1) 
-		}
-
-	if (method == "points") {
 		jit <- position_jitter(width = 0.05, height = 0.0)
-		p <- p + layer(geom = "jitter", position = jit)		}
-		
-	if (method == "box") {
-		p <- p + geom_boxplot(width = 0.2, outlier.size = 0, fill = "grey90")
-		}
+		if (TwoFac) {
+			p <- p + geom_jitter(data = data,
+			aes_string(x = fac1, y = res, color = fac2, group = fac2),
+			position = jit, size = 1.0)
+			}
+		if (!TwoFac) {
+			p <- p + geom_jitter(data = data,
+			aes_string(x = fac1, y = res, color = fac1),
+			position = jit, size = 1.0)
+			}
+		}	
+
+	# Boxplot does its own summary
 	
-	if (method == "sem") {
-		p <- p + stat_summary(fun.data = "seXy")		}
-	
-	if (method == "sem95") {
-		p <- p + stat_summary(fun.data = "seXy95")
+	if (TwoFac) {
+		if (method == "box") { # Does not work correctly for TwoFac
+			p <- p + geom_boxplot(data = data, width = 0.2,
+			aes_string(x = fac1, y = res, color = fac2, group = fac2))
+			}
 		}
 
-	if (method == "mad") {
-		p <- p + stat_summary(fun.data = "seXyMad")
-		}
-		
-	if (method == "iqr") {
-		p <- p + stat_summary(fun.data = "seXyIqr")
+	if (!TwoFac) {
+		if (method == "box") {
+			p <- p + geom_boxplot(data = data, width = 0.2,
+			aes_string(x = fac1, y = res, color = fac1))
+			}
 		}
 
-	# now add the common decorations & modifications
+	# All plotting below here uses this summary data
+	# Prepare the summary data (required for all other options)
+	# Due to a bug in ggplot2_0.9.3, we must calc some quantities
+	# and put them in a separate data frame for a new aesthetic
+	# Slightly wasteful to compute all when you may not use all, but...
+
+	if (TwoFac) {
+		meany <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = mean)
+		medy <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = median)
+		sexy <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXy)
+		sexy95 <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXy95)
+		sexymad <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXyMad)
+		sexyiqr <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXyIqr)
+		sumDat <- cbind(meany, medy[,3], sexy[[3]][,c(2,3)], sexy95[[3]][,c(2,3)],
+			sexymad[[3]][,c(2,3)], sexyiqr[[3]][,c(2,3)])
+		names(sumDat) <- c("factor1", "factor2", "mean", "median",
+			"semL", "semU", "sem95L", "sem95U", "madL", "madU",
+			"iqrL", "iqrU")
+#		print(sumDat)
+
+		# Now add summary layers as requested
+		# These are aesthetic #2, using a new data set
 	
-	p <- p  + scale_colour_manual(values = fac1cols) +
+		# sumDat2 <- melt(sumDat, id.vars = c("factor1", "factor2"))
+		# print(sumDat2)
+		
+		if (method == "sem") {
+			sumDat2 <- sumDat[,c(1,2,3,5,6)]
+			sumDat2 <- melt(sumDat2, id.vars = c("factor1", "factor2"), measure.vars = c("mean", "semL", "semU"))
+			sumDat2 <- dcast(sumDat2, factor1 + factor2 ~ variable)
+			print(sumDat2)
+			p <- p + geom_pointrange(data = sumDat2,
+				aes(x = factor1, y = mean, ymin = semL, ymax = semU,
+				color = factor2, group = factor2))
+			}
+	
+		if (method == "sem95") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = mean, ymin = sem95L, ymax = sem95U,
+				color = factor2, group = factor2))
+			}
+	
+		if (method == "mad") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = median, ymin = madL, ymax = madU,
+				color = factor2, group = factor2))
+			}
+	
+		if (method == "iqr") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = median, ymin = iqrL, ymax = iqrU,
+				color = factor2, group = factor2))
+			}
+	} # end of TwoFac = TRUE
+
+	if (!TwoFac) { # These all work
+		meany <- aggregate(data[,res] ~ data[,fac1], data, FUN = mean)
+		medy <- aggregate(data[,res] ~ data[,fac1], data, FUN = median)
+		sexy <- aggregate(data[,res] ~ data[,fac1], data, FUN = seXy)
+		sexy95 <- aggregate(data[,res] ~ data[,fac1], data, FUN = seXy95)
+		sexymad <- aggregate(data[,res] ~ data[,fac1], data, FUN = seXyMad)
+		sexyiqr <- aggregate(data[,res] ~ data[,fac1], data, FUN = seXyIqr)
+		sumDat <- cbind(meany, medy[,2], sexy[[2]][,c(2,3)], sexy95[[2]][,c(2,3)],
+			sexymad[[2]][,c(2,3)], sexyiqr[[2]][,c(2,3)])
+		names(sumDat) <- c("factor1", "mean", "median",
+			"semL", "semU", "sem95L", "sem95U", "madL", "madU",
+			"iqrL", "iqrU")
+	
+		# Now add summary layers as requested
+		# These are aesthetic #2, using a new data set
+	
+		if (method == "sem") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = mean, ymin = semL, ymax = semU,
+				color = factor1))
+			}
+	
+		if (method == "sem95") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = mean, ymin = sem95L, ymax = sem95U,
+				color = factor1))
+			}
+	
+		if (method == "mad") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = median, ymin = madL, ymax = madU,
+				color = factor1))
+			}
+	
+		if (method == "iqr") {
+			p <- p + geom_pointrange(data = sumDat,
+				aes(x = factor1, y = median, ymin = iqrL, ymax = iqrU,
+				color = factor1))
+			}
+	} # End of TwoFac = FALSE
+	
+	# Set up the color scheme & legend
+	
+	if (TwoFac) if (is.null(cols)) cols <- brewer.pal(length(levels(data[,fac2])), "Set1")
+	if (!TwoFac) if (is.null(cols)) cols <- brewer.pal(length(levels(data[,fac1])), "Set1")
+	
+	p <- p  + scale_colour_manual(name = "", values = cols) +
 		theme(axis.text.x = element_text(colour = "black"),
-		axis.ticks = element_blank(), legend.position = "none", ...)
+		axis.text.y = element_text(colour = "black"),
+		axis.ticks = element_blank())		
 		
-	# now add labels and fix limits (modified from qplot)
+	# Now add labels and fix limits (modified from qplot)
 	
     if (!is.null(title)) p <- p + labs(title = title)
     if (!is.null(xlab)) p <- p + labs(xlab = xlab)
@@ -78,18 +198,15 @@ function(data = NULL, res = NULL,
 #    if (exists("xlim")) p <- p + xlim(xlim)
 #    if (exists("ylim")) p <- p + ylim(ylim)
 
-	if (!identical(fac2, NULL)) {
-		facCounts <- count(data, vars = c(fac2, fac1))
-		} else {
-			facCounts <- count(data, vars = fac1)
-			}
+	if (TwoFac) facCounts <- count(data, vars = c(fac2, fac1))
+	if (!TwoFac) facCounts <- count(data, vars = fac1)
 	
-	facCounts$label <- paste("n = ", facCounts$freq , sep = "")
-	facCounts$x <- seq(1, length(levels(data[,fac1])), by = 1)
-	facCounts$y <- min(data$res) - 0.1*diff(range(data$res))
+	# facCounts$label <- paste("n = ", facCounts$freq , sep = "")
+	# facCounts$x <- seq(1, length(levels(data[,fac1])), by = 1) # probably not right
+	# facCounts$y <- min(data$res) - 0.1*diff(range(data$res))
 
-	p <- p + geom_text(aes(x, y, label = label), 
-		color = "black", size = 4.0, data = facCounts)
+	# p <- p + geom_text(aes(x, y, label = label), 
+		# color = "black", size = 4.0, data = facCounts)
 
 	invisible(p)
 	}
--- R/rxnNorm.R
+++ R/rxnNorm.R
@@ -1,6 +1,5 @@
 rxnNorm <-
-function(data = NULL, res = NULL,
-	fac1 = NULL, fac2 = NULL, fac2cols = NULL,
+function(formula = NULL, data = NULL, cols = NULL,
 	freckles = FALSE, type = "connect",
 	method = c("sem", "iqr", "mad", "box", "sem95"),
 	table = NULL, xlab = NULL, ylab = NULL, title = NULL, ...) {
@@ -14,9 +13,19 @@ function(data = NULL, res = NULL,
 # fac2 must be a factor, but fac1 can be either factor or numeric
 # Everything related to a particular level of fac2 should be colored the same
 	
-	if (is.null(fac2cols)) stop("You need to supply fac2cols")
+	# Check and process the formula
 	
-	# 1st, reduce the df and clean it of NAs
+	if (is.null(formula)) stop("Formula not given")
+	if (!is.formula(formula)) stop("Invalid formula specification")
+	if (!length(formula) == 3) stop("Formula must include both LHS and RHS terms, e.g.: response ~ factor1*factor2")
+	if (!length(all.vars(formula[[3]])) == 2) stop("Formula must include interaction terms, e.g.: response ~ factor1*factor2")
+	if (!formula[[3]][[1]] == "*") stop("Formula must include interaction terms, e.g.: response ~ factor1*factor2")
+
+	res <- as.character(formula[[2]])
+	fac1 <- as.character(formula[[3]][2])
+	fac2 <- as.character(formula[[3]][3])
+	
+	# Reduce the df and clean it of NAs
 	# otherwise there is trouble with the counts
 	# plus this keeps the ggplot2 object size down
 	
@@ -24,64 +33,110 @@ function(data = NULL, res = NULL,
 	data <- data[, keep]
 	data <- na.omit(data)
 
-	# Specify the data & aesthetics
-		
-	p <- ggplot(data, aes_string(x = fac1, y = res, color = fac2,
-		group = fac2))
+	if (!is.factor(data[,fac2])) stop("factor2 was not actually of type factor")
 
-	# now add layers as requested
+	# Initialize + add the points, jittered or not
+	# This is aesthetic #1
+	
+	p <- ggplot()
+	
+	if (!freckles) {
+		p <- p + geom_point(data = data, 
+		aes_string(x = fac1, y = res, color = fac2, group = fac2))
+		}
 
 	if (freckles) {
 		jit <- position_jitter(width = 0.05, height = 0.0)
-		p <- p + geom_jitter(position = jit, size = 1.0) 
+		p <- p + geom_jitter(data = data,
+		aes_string(x = fac1, y = res, color = fac2, group = fac2),
+		position = jit, size = 1.0)
 		}
+
+	# Set up the color scheme & legend
 	
-	if (method == "box") { # this doesn't work right
-		p <- p + geom_boxplot(width = 0.2)
-		if (type == "connect") {
-			p <- p + stat_summary(fun.y = "median", geom = "line")
-				}
-		}
-		
-	if (method == "sem") {
-		p <- p + stat_summary(fun.data = "seXy")
-		if ((type == "connect") | (type == "anova")) {
-			p <- p + stat_summary(fun.y = "mean", geom = "line")
-				}
-		}
-		
-	if (method == "sem95") {
-		p <- p + stat_summary(fun.data = "seXy95")
-		if ((type == "connect") | (type == "anova")) {
-			p <- p + stat_summary(fun.y = "mean", geom = "line")
-				}
-		}
-		
-	if (method == "mad") {
-		p <- p + stat_summary(fun.data = "seXyMad")
-		if ((type == "connect") | (type == "anova")) {
-			p <- p + stat_summary(fun.y = "median", geom = "line")
-				}
-		}
-		
-	if (method == "iqr") {
-		p <- p + stat_summary(fun.data = "seXyIqr")
-		if ((type == "connect") | (type == "anova")) {
-			p <- p + stat_summary(fun.y = "median", geom = "line")
-				}
-		}
+	if (is.null(cols)) cols <- brewer.pal(length(levels(data[,fac2])), "Set1")
 	
-	if (type == "fitLine") {
-		p <- p + geom_smooth(method = "lm", se = FALSE)
-		
-		}
-		
-	# now add the common decorations & modifications
-	
-	p <- p  + scale_colour_manual(name = "", values = fac2cols) +
+	p <- p  + scale_colour_manual(name = "", values = cols) +
 		theme(axis.text.x = element_text(colour = "black"),
 		axis.text.y = element_text(colour = "black"),
 		axis.ticks = element_blank())		
+
+	# Do fitLine next as it uses the same aesthetics
+	
+	if (type == "fitLine") {
+		p <- p + geom_smooth(data = data, se = FALSE, method = "lm",
+		aes_string(x = fac1, y = res, color = fac2, group = fac2))
+		}
+
+	# Boxplot does its own summary
+	
+	if (method == "box") { # Does not work correctly
+		p <- p + geom_boxplot(data = data, width = 0.2,
+		aes_string(x = fac1, y = res, color = fac2, group = fac2))
+		}
+
+	# All plotting below here uses this summary data
+	# Prepare the summary data (required for all other options)
+	# Due to a bug in ggplot2_0.9.3, we must calc some quantities
+	# and put them in a separate data frame for a new aesthetic
+	# Slightly wasteful to compute all when you may not use all, but...
+
+	meany <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = mean)
+	medy <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = median)
+	sexy <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXy)
+	sexy95 <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXy95)
+	sexymad <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXyMad)
+	sexyiqr <- aggregate(data[,res] ~ data[,fac1]*data[, fac2], data, FUN = seXyIqr)
+	sumDat <- cbind(meany, medy[,3], sexy[[3]][,c(2,3)], sexy95[[3]][,c(2,3)],
+		sexymad[[3]][,c(2,3)], sexyiqr[[3]][,c(2,3)])
+	names(sumDat) <- c("factor1", "factor2", "mean", "median",
+		"semL", "semU", "sem95L", "sem95U", "madL", "madU",
+		"iqrL", "iqrU")
+
+	# Now add summary layers as requested
+	# These are aesthetic #2, using a new data set
+
+	if (method == "sem") {
+		p <- p + geom_pointrange(data = sumDat,
+			aes(x = factor1, y = mean, ymin = semL, ymax = semU,
+			color = factor2, group = factor2))
+		}
+
+	if (method == "sem95") {
+		p <- p + geom_pointrange(data = sumDat,
+			aes(x = factor1, y = mean, ymin = sem95L, ymax = sem95U,
+			color = factor2, group = factor2))
+		}
+
+	if (method == "mad") {
+		p <- p + geom_pointrange(data = sumDat,
+			aes(x = factor1, y = median, ymin = madL, ymax = madU,
+			color = factor2, group = factor2))
+		}
+
+	if (method == "iqr") {
+		p <- p + geom_pointrange(data = sumDat,
+			aes(x = factor1, y = median, ymin = iqrL, ymax = iqrU,
+			color = factor2, group = factor2))
+		}
+	
+	# Now add the appropriate lines
+	# New layers, but using aes #2 explicitly
+		
+	if ((type == "connect") | (type == "anova")) {
+		
+		if ((method == "sem") | (method == "sem95")) {
+			p <- p + geom_line(data = sumDat,
+			aes(x = factor1, y = mean,
+			color = factor2, group = factor2))
+			}
+			
+		if ((method == "mad") | (method == "iqr")) {
+			p <- p + geom_line(data = sumDat,
+			aes(x = factor1, y = median,
+			color = factor2, group = factor2))
+			}
+		}
 
 	# now add labels and fix limits (modified from qplot)
 	
@@ -100,13 +155,16 @@ function(data = NULL, res = NULL,
 			counts <- count(data, vars = c(fac2, fac1))
 			colnames(counts) <- c(fac2, fac1, "count")
 
-			myt <- tableGrob(counts, show.box = TRUE,
-				show.rownames = FALSE, show.colnames = TRUE,
-				show.csep = TRUE, show.rsep = TRUE,
-				separator = "black", gp = gpar(cex = table[3]))
-
-			p <- p + annotation_custom(myt, xmin = table[1]-0.5, xmax = table[1]+0.5,
-				ymin = table[2]-0.5, ymax = table[2]+0.5)
+			# myt <- tableGrob(counts, show.box = TRUE,
+				# show.rownames = FALSE, show.colnames = TRUE,
+				# show.csep = TRUE, show.rsep = TRUE,
+				# separator = "black", gp = gpar(cex = table[3]))
+			
+			# p <- p + annotation_custom(grob=circleGrob(r=unit(1,"npc")), xmin = table[1]-0.5, xmax = table[1]+0.5,
+				# ymin = table[2]-0.5, ymax = table[2]+0.5)
+			#p <- p + annotation_custom(circleGrob())
+			pp <- qplot(1, 1, geom = "blank") + annotation_custom(circleGrob())
+			p <- p + annotation_custom(ggplotGrob(pp))
 			}
 
 		if (type == "anova") {
